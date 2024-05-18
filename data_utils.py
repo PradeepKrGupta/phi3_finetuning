7c915a51303f078264000d0be6cd206011b6bd47
Pradeep <pradeeprauniyarofficial@gmail.com>
2024-05-18 20:58:44 +0530

    doing some modification

--- data_utils.py
+++ data_utils.py
@@ -3,6 +3,7 @@ import datasets
 import config
 
 file_path = "./phiDataset.csv"
+
 def load_local_dataset(file_path):
     print("Loading dataset from - ", file_path, "...")
     dataset_df = pd.read_csv(file_path)
@@ -10,22 +11,18 @@ def load_local_dataset(file_path):
 
 def prepare_prompts_responses(dataset_df):
     print("Preparing Prompt and Assistant....")
-    user_prompters = dataset_df[(dataset_df.role == "prompter")]
-    user_prompters = user_prompters.set_index("message_id")
-    assistants = dataset_df[(dataset_df.role == "assistant") & (dataset_df["rank"] == 0.0)]
-    
     prompts_responses = []
-    for _, record in assistants.iterrows():
-        prompt_text = user_prompters.loc[record.parent_id, 'text']
-        prompt_response = "### Human: " + prompt_text + " ### Assistant: " + record['text']
-        prompts_responses.append(prompt_response)
-    assistants[config.DATASET_TEXT_FIELD] = prompts_responses
+    for _, record in dataset_df.iterrows():
+        prompt_text = record['Hook']
+        response_text = record['Body']
+        prompt_response = "### Human: " + prompt_text + " ### Assistant: " + response_text
+        prompts_responses.append({config.DATASET_TEXT_FIELD: prompt_response})
     
-    return assistants
+    return pd.DataFrame(prompts_responses)
 
 def preparedata(mode):
     print("Preparing data for - ", mode, "...")
     dataset_df = load_local_dataset(config.DATASET_PATH)
-    prompts_responses = prepare_prompts_responses(dataset_df)
-    prompts_responses_dataset = datasets.Dataset.from_pandas(prompts_responses)
+    prompts_responses_df = prepare_prompts_responses(dataset_df)
+    prompts_responses_dataset = datasets.Dataset.from_pandas(prompts_responses_df)
     return prompts_responses_dataset
